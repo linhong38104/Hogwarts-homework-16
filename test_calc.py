3b968ed525c26ac206815ce6c2d3c4dafd7bcd78
linhong38104 <527696220@qq.com>
2020-12-15 17:37:40 +0800

    20201215

--- test_calc.py
+++ test_calc.py
@@ -1,6 +1,6 @@
 import pytest
 from pythoncode.calculator import Calculator
-
+import yaml
 
 class TestCalc:
     def setup_class(self):
@@ -13,26 +13,24 @@ class TestCalc:
     def teardown_method(self):
         print("结束计算")
 
-    @pytest.mark.parametrize("a,b,expect",[
-        (1,2,3),(-1,-2,-3),(100,300,400),(6,6.6,12.6)
-    ],ids=["int","minus","bigint","int and float"])
+
+    @pytest.mark.parametrize("a,b,expect",yaml.safe_load(open("./data.yml"))["add_datas"],
+                             ids=yaml.safe_load(open("./data.yml"))["myid"])
     def test_add(self,a,b,expect):
         assert expect == self.calc.add(a,b)
 
-    @pytest.mark.parametrize("a,b,expect",[
-        (2,1,1),(-1,-2,1),(100,300,-200),(6,6.6,-0.6)
-    ],ids=["int","minus","bigint","int and float,need to round?"])
+
+    @pytest.mark.parametrize("a,b,expect",yaml.safe_load(open("./data.yml"))["sub_datas"],
+                             ids=yaml.safe_load(open("./data.yml"))["myid"])
     def test_sub(self,a,b,expect):
         assert expect == self.calc.sub(a,b)
 
-    @pytest.mark.parametrize("a,b,expect",[
-        (2,1,2),(-1,-2,2),(100,300,30000),(1,0,0),(6,-6.6,-39.6)
-    ],ids=["int","minus","bigint","result=0","int and float,need to round?"])
+    @pytest.mark.parametrize("a,b,expect",yaml.safe_load(open("./data.yml"))["mul_datas"],
+                             ids=yaml.safe_load(open("./data.yml"))["myid_mul"])
     def test_mul(self,a,b,expect):
         assert expect == self.calc.mul(a,b)
 
-    @pytest.mark.parametrize("a,b,expect",[
-        (2,1,2),(-4,-2,2),(600,300,2),(3.6,1.2,3),(1,0,'ERR')
-    ],ids=["int","minus","bigint","float","ERR=0"])
+    @pytest.mark.parametrize("a,b,expect",yaml.safe_load(open("./data.yml"))["div_datas"],
+                             ids=yaml.safe_load(open("./data.yml"))["myid_div"])
     def test_div(self,a,b,expect):
         assert expect == self.calc.div(a,b)
